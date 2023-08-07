9c9cfb3a24d3e4abdce4aa8de27bfcc16eb7bbde
Ali God <aligod8001@gmail.com>
2023-08-07 11:22:03 +0330

    [UPD] update next.config.js file.

--- next.config.js
+++ next.config.js
@@ -1,6 +1,24 @@
 /** @type {import('next').NextConfig} */
+
+const isGithubActions = process.env.GITHUB_ACTIONS || false
+
+let assetPrefix = ''
+let basePath = '/'
+
+if (isGithubActions) {
+  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
+
+  assetPrefix = `/${repo}/`
+  basePath = `/${repo}`
+}
+
 const nextConfig = {
+    assetPrefix: assetPrefix,
+    basePath: basePath,
     output: 'export',
+    images: {
+        loader: 'imgix',
+    },
 };
   
 
